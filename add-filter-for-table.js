361d36b58f011dc3380ecfb4903aef6ffeb1fa44
duong273 <duong273@users.noreply.github.com>
2025-04-28 15:43:19 +0700

    Update add-filter-for-table.js

--- add-filter-for-table.js
+++ add-filter-for-table.js
@@ -1,26 +1,51 @@
-// Tải DataTables từ CDN
-document.write('<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">');
-document.write('<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>');
-document.write('<script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>');
+"use strict";
 
-// Thiết lập DataTables với ngôn ngữ tiếng Việt
-$(document).ready(function() {
-  $('table.table').DataTable({
-    "pageLength": 100,
-    "language": {
-      "sProcessing":   "Đang xử lý...",
-      "sLengthMenu":   "Hiển thị _MENU_ mục",
-      "sZeroRecords":  "Không tìm thấy kết quả",
-      "sInfo":         "Đang hiển thị _START_ đến _END_ của _TOTAL_ mục",
-      "sInfoEmpty":    "Đang hiển thị 0 đến 0 của 0 mục",
-      "sInfoFiltered": "(đã lọc từ _MAX_ mục)",
-      "sSearch":       "Tìm kiếm:",
-      "oPaginate": {
-        "sFirst":    "Đầu tiên",
-        "sPrevious": "Trước",
-        "sNext":     "Tiếp theo",
-        "sLast":     "Cuối cùng"
-      }
-    }
+function loadScript(url, callback) {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = url;
+  script.onload = callback;
+  script.onerror = function() {
+    console.error("Không thể tải script: " + url);
+  };
+  document.head.appendChild(script);
+}
+
+function loadStyle(url) {
+  var link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = url;
+  document.head.appendChild(link);
+}
+
+// Load CSS DataTables
+loadStyle("https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css");
+
+// Bắt đầu load JS
+loadScript("https://code.jquery.com/jquery-3.6.0.min.js", function() {
+  console.log("Đã tải xong jQuery");
+  loadScript("https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js", function() {
+    console.log("Đã tải xong DataTables");
+
+    // Khi cả jQuery và DataTables đã sẵn sàng
+    $(document).ready(function() {
+      $('table').DataTable({
+        pageLength: 100, // Mặc định 100 dòng
+        language: {
+          "sLengthMenu": "Hiển thị _MENU_ dòng mỗi trang",
+          "sZeroRecords": "Không tìm thấy dữ liệu",
+          "sInfo": "Đang hiển thị _START_ đến _END_ của _TOTAL_ dòng",
+          "sInfoEmpty": "Không có dữ liệu",
+          "sInfoFiltered": "(lọc từ _MAX_ dòng)",
+          "sSearch": "Tìm kiếm:",
+          "oPaginate": {
+            "sFirst": "Đầu",
+            "sPrevious": "Trước",
+            "sNext": "Tiếp",
+            "sLast": "Cuối"
+          }
+        }
+      });
+    });
   });
 });
